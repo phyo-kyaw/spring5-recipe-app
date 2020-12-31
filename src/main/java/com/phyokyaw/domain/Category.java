fac57323a123161f1d27aa976e199597fbfc7dc2
phyo-kyaw <phy0ky6w@gmail.com>
2021-01-01 00:32:44 +1100

    Many To Many JPA Relationship

---
+++ src/main/java/com/phyokyaw/domain/Category.java
@@ -0,0 +1,41 @@
+package com.phyokyaw.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipeSet;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipeSet() {
+        return recipeSet;
+    }
+
+    public void setRecipeSet(Set<Recipe> recipeSet) {
+        this.recipeSet = recipeSet;
+    }
+}
--- src/main/java/com/phyokyaw/domain/Recipe.java
+++ src/main/java/com/phyokyaw/domain/Recipe.java
@@ -32,6 +32,12 @@ public class Recipe {
     @OneToOne( cascade =  CascadeType.ALL)
     private Notes notes;
 
+    @ManyToMany
+    @JoinTable(name= "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
@@ -112,11 +118,27 @@ public class Recipe {
         this.image = image;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
     public Notes getNotes() {
         return notes;
     }
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategorySet(Set<Category> categories) {
+        this.categories = categories;
     }
 }
